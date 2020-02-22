622a3fd10aa0c7e1f28b6c14f6c7c07d4f119098
William Situ <william.situ@william.situ>
2020-02-22 17:01:39 -0500

    unfinished backend filtering

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Choose your minions based on your budget
 ## How to Run:
 Run the app with the following command:
 
-	$ node app.js
+	$ node main.js
 
--- main.js
+++ main.js
@@ -7,15 +7,28 @@ app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-app.get('/:state/:skilltype/:level', function (req, res) {
+passesReqs(person, states, skills, skillLevels){
+    if (states.indexOf(person.address.state) == -1) return false;
+    // TODO: more
+}
+
+app.post('/filter', function (req, res) {
     const request = require('request')
+    // TODO: choose those that actually applied for the job
     request.get('https://hackicims.com/api/v1/companies/141/people', {
         auth: {
             bearer: '996855af2954a57fe8e0285a74136151aa7c3b388cf075cf05b127202217dcebacf3558a7fcd6de1943769da66a196d165bdb8286ffb20482fd702c2cc9a6f98'
         },
         json: true
         }, (error, response, body)=>{
-            res.send(body)
+
+            // filter over here
+            for (int i = body.length - 1; i <= 0; i--){
+                if (!passesReqs(body[i], states, skills, skillLevels))
+                    body.splice(i, 1);
+            }
+
+            res.send(body[0])
     })
 })
   
