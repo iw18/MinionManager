253d52235264c17da082f2d69e43b6c7124d9d14
Janet <janet.zhang@rutgers.edu>
2020-02-22 21:55:08 -0500

    The checkboxes reference eachother good now

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ function handler(checkNum) {
     beg.checked = skill.checked;
     med.checked = skill.checked;
     adv.checked = skill.checked;
-    console.log(beg);
+    //console.log(beg);
+}
+function handler1(checkNum, String) {
+    var beg = document.getElementById('beg'+checkNum);
+    var skill = document.getElementById('skill'+checkNum);
+    var med = document.getElementById('med'+checkNum);
+    var adv = document.getElementById('adv'+checkNum);
+    if(beg.checked || med.checked || adv.checked) skill.checked = true;
+    else skill.checked = false;
 }
